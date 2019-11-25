0dda8fe5b0717593b7bd6bd705339ca70d360e26
John <phodgesfl@gmail.com>
2019-11-24 20:46:34 -0500

    im so confused

--- server.js
+++ server.js
@@ -13,6 +13,40 @@
 
  connection.connect(function(err) {
      if (err) throw err;
-     console.log("connected as id" + connection.threadId);
-     connection.end();
+     Start();
+
  });
+
+ function start() {
+
+     inquirer.prompt({
+
+     })
+ }
+
+ function AddEmployee() {
+     console.log("Adding Employee... \n");
+     let query = connection.query(
+         "Add Employee to Employee Table?", {
+             First_name: "",
+             Last_name: "",
+             Role_id: "",
+             Manager_id: ""
+
+         },
+         (err, res) => {
+             if (err) throw err;
+             console.log(res.affectedRows + "product inserted!\n");
+             updateEmployee();
+         }
+     )
+ }
+
+ function updateEmployee() {
+     console.log("Updating Employee Table...\n");
+     let query = connection.query(
+         "Upadte Employee Table?", [
+             {}
+         ]
+     )
+ }
